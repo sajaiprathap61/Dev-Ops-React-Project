9a4d5083fe275de6fcc3780283af9587da4643c4
Sajai Prathap <sajaiprathapkp@gmail.com>
2025-02-18 22:05:58 +0530

    build.sh changes

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 @!/bin/bash
 
 # Build Docker image
-docker build -t app:dev .
+sudo docker build -t app:dev .
 
 # Tag and push the dev image to the public Docker Hub
-docker tag app:dev sajaiprathap/dev:latest
-docker push sajaiprathap/dev:latest
+sudo docker tag app:dev sajaiprathap/dev:latest
+sudo docker push sajaiprathap/dev:latest
 
 # Build Docker image for production private repository
-docker build -t app:prod .
+sudo docker build -t app:prod .
 
 # Tag and push the production image to the private Docker Hub repository
-docker tag app:prod sajaiprathap/prod:latest
-docker push sajaiprathap/prod:latest
+sudo docker tag app:prod sajaiprathap/prod:latest
+sudo docker push sajaiprathap/prod:latest
