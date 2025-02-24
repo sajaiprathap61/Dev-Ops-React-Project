509840f6ed40356c2c2fe675ef677f66aff920c9
sajaiprathap61 <sajaiprathap61@gmail.com>
2025-02-24 11:35:45 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,11 +5,11 @@ docker build -t sajaiprathap/dev .
 
 # Tag and push the dev image to the public Docker Hub
 # docker tag app:dev sajaiprathap/dev:latest
-docker push sajaiprathap/dev:latest
+docker push sajaiprathap/dev
 
 # Build Docker image for production private repository
 docker build -t sajaiprathap/prod .
 
 # Tag and push the production image to the private Docker Hub repository
 # docker tag app:prod sajaiprathap/prod:latest
-docker push sajaiprathap/prod:latest
+docker push sajaiprathap/prod
