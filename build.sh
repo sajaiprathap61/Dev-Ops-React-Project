28768d0b3f31e6c6e0f58b82261706f6aa4d206d
sajaiprathap61 <sajaiprathap61@gmail.com>
2025-02-24 11:30:38 +0530

    Update build.sh
    Commented the tag command

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Build Docker image
-docker build -t app:dev .
+docker build -t sajaiprathap/dev .
 
 # Tag and push the dev image to the public Docker Hub
-docker tag app:dev sajaiprathap/dev:latest
+# docker tag app:dev sajaiprathap/dev:latest
 docker push sajaiprathap/dev:latest
 
 # Build Docker image for production private repository
-docker build -t app:prod .
+docker build -t sajaiprathap/prod .
 
 # Tag and push the production image to the private Docker Hub repository
-docker tag app:prod sajaiprathap/prod:latest
+# docker tag app:prod sajaiprathap/prod:latest
 docker push sajaiprathap/prod:latest
